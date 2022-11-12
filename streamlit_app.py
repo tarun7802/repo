52c07b2f0e285f6ce61555fcd71eb1a9bf6cacdb
tarun7802 <118069849+tarun7802@users.noreply.github.com>
2022-11-12 12:16:54 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,4 +3,6 @@ import snowflake.connector
 
 streamlit.title("My Parents new healthy dinner")
 streamlit.header("Breakfast Menu")
-streamlit.text('Omega 3 and Blueberry oatmeal')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
