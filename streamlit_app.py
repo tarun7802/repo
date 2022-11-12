8b5df883810105b0e4ff83d693ed2bd869b49c6e
tarun7802 <118069849+tarun7802@users.noreply.github.com>
2022-11-12 14:07:44 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,7 +3,7 @@ import snowflake.connector
 import pandas
 import requests
 
-streamlit.title("MANVIK Please learn Python, It very intresting, I made this app in Python programming !!"
+streamlit.title("MANVIK Please learn Python, It very intresting, I made this app in Python programming !!")
 streamlit.header("Breakfast Favorites")
    
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
