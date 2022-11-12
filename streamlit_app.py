30a6d35ca2a1b6e41481aca5132c37786ea519bd
tarun7802 <118069849+tarun7802@users.noreply.github.com>
2022-11-12 12:23:14 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import snowflake.connector
+import pandas
 
 streamlit.title("My Parents new healthy dinner")
 streamlit.header("Breakfast Favorites")
@@ -9,3 +10,6 @@ streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔Hard-Boiled Free-Range Egg')
 streamlit.text('🥑🍞 Avocado Tost')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+streamlit.dataframe(my_fruit_list)
